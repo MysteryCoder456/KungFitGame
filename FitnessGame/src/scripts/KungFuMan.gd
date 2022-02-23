7b7fe006885371368a82b6ec1b0f7099e122cffd
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-02-23 22:59:49 +0400

    played state machine and animations

--- FitnessGame/src/scripts/KungFuMan.gd
+++ FitnessGame/src/scripts/KungFuMan.gd
@@ -1,29 +1,68 @@
 extends KinematicBody2D
 
-var direction_strings = ["up", "down", "left", "right"]
-
+const DIRECTION_STRINGS = ["up", "down", "left", "right"]
 enum Direction {
-	UP = 0,
+	UP = 0
 	DOWN = 1
 	LEFT = 2
 	RIGHT = 3
 }
 
+enum State {
+	IDLE
+	RUNNING
+}
+
 export var speed: float
+
 var velocity = Vector2.ZERO
 var direction = Direction.DOWN
+var state = State.IDLE
+
+onready var animated_sprite = $AnimatedSprite
 
 
 func _physics_process(delta: float):
 	velocity = get_movement_vel()
+
+	if velocity.y < 0:
+		direction = Direction.UP
+	elif velocity.y > 0:
+		direction = Direction.DOWN
+	elif velocity.x < 0:
+		direction = Direction.LEFT
+	elif velocity.x > 0:
+		direction = Direction.RIGHT
+
+	var dir_str = DIRECTION_STRINGS[direction]
+
+	match state:
+		State.IDLE:
+			if velocity != Vector2.ZERO:
+				state = State.RUNNING
+
+			animated_sprite.play("idle_%s" % dir_str)
+
+		State.RUNNING:
+			if velocity == Vector2.ZERO:
+				state = State.IDLE
+
+			animated_sprite.play("running_%s" % dir_str)
+
 	velocity = move_and_slide(velocity)
 
 
 func get_movement_vel() -> Vector2:
 	var new_velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_up"):
+		new_velocity = Vector2.UP
+	elif Input.is_action_pressed("move_down"):
+		new_velocity = Vector2.DOWN
+	elif Input.is_action_pressed("move_left"):
+		new_velocity = Vector2.LEFT
+	elif Input.is_action_pressed("move_right"):
+		new_velocity = Vector2.RIGHT
 
-	new_velocity.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	new_velocity.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	new_velocity *= speed
-
 	return new_velocity
